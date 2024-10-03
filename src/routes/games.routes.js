b64226d408d6045876441647cb6e8e5d64ca9e08
Ivan Siles <silesivansalustiano@gmail.com>
2024-10-03 12:17:14 -0300

    1.1.1/customrouter/get&getById

--- src/routes/games.routes.js
+++ src/routes/games.routes.js
@@ -34,26 +34,3 @@ class GamesRouterManager extends CustomRouter {
 const gamesRouter = new GamesRouterManager().getRouter();
 
 export default gamesRouter;
-
-
-
-router.get("/realtimeproducts", (req, res) => {
-    toSendObject = ProductManagerFS.readFileAndSave();
-    res.render('realTimeProducts', {toSendObject: toSendObject});
-  });
-  router.post("/realtimeproducts", uploader.single("archivo"), (req, res) => {
-    const socketServer = req.app.get("socketServer");  
-    const {newProduct, productAction} = JSON.parse(req.body.json); 
-    const {id} = newProduct;
-    if (productAction == "add") {
-      let toAddProduct = {...newProduct, thumbnail: req.file.filename, status: true};
-      ProductManagerFS.addProduct(toAddProduct);
-      let toAddId = ProductManagerFS.readFileAndSave()[ProductManagerFS.readFileAndSave().length-1]._id;
-      socketServer.emit("addConfirmed", {msg: "Producto agregado.", toAddId});
-    } else if (productAction == "delete") {
-      ProductManagerFS.deleteProductById(id);
-      
-      socketServer.emit("deleteConfirmed", {msg: `Producto de ID ${id} eliminado.`, pid: id});
-    }
-    res.render('realTimeProducts', {toSendObject: toSendObject});
-  });
